fabb82697c0c2d40e9f67e2f3b9f4e27cd9aa849
Oni Kehinde <kehindeoni89@gmail.com>
2023-02-16 23:40:13 +0100

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 /**
  * main - Entry point
+ *
  * Description - 'C Program that prints outputs with puts function'
- * return: 0
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle, n")
+	puts("\"Programming is like building a multilingual puzzle, n");
 		return (0);
+
 }
