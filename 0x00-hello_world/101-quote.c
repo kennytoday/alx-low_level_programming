f5ce2c5de2e462f6a20f22c640c0285456645bc5
Oni Kehinde <kehindeoni89@gmail.com>
2023-02-17 11:12:17 +0100

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd>
+/**
+ * main - Entry point
+ *
+ * description - C program that prints standard output
+ *
+ * return: 1 (Success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
