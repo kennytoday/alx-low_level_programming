32a40bc92be5398301ea3cafc70623e3dda7dd0f
Oni Kehinde <kehindeoni89@gmail.com>
2023-02-17 17:29:21 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description - 'C program in lower case'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char kenny;
+
+	for (kenny = a; kenny <= z; kenny++)
+	{
+		putchar(kenny);
+	}
+	putchar("\n");
+	return (0);
+}
