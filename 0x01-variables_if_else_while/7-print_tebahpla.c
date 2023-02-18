b6481dbb2cacf3570e754785075cb6cb39300fe1
Oni Kehinde <kehindeoni89@gmail.com>
2023-02-18 10:04:58 +0100

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int jb;
+
+	for (jb = 'z'; jb <= 'a'; jb++)
+
+		putchar(jb);
+	putchar('\n');
+	return (0);
+}
