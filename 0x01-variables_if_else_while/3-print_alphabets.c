08d4a585b1f6d4560a501dc42c916c2f7b9abd57
Oni Kehinde <kehindeoni89@gmail.com>
2023-02-17 19:00:40 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchat(ch);
+	puchar('\n');
+	return (0);
+}
