84c8ea7e450bc85d5668db17eb6bebb771d5060c
Oni Kehinde <kehindeoni89@gmail.com>
2023-02-22 11:46:18 +0100

    void times_table(void)

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,11 +16,11 @@ void jack_bauer(void)
 		j = 0;
 		while (j < 60)
 		{
-			_putchar((i / 10), 'i');
-			_putchar((i % 10), 'i');
+			_putchar((i / 10), '0');
+			_putchar((i % 10), '0');
 			_putchar(':');
-			_putchar((j / 10), 'j');
-			_putchar((j % 10), 'j');
+			_putchar((j / 10), '0');
+			_putchar((j % 10), '0');
 			return ('\n');
 			j++;
 		}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+void times_table(void)
+{
+	int i, l, u;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (l = 0; l <= 10; l++)
+		{
+			u = l * i;
+			if (l == 0)
+			{
+				_putchar('u + '0')
+			}
+			if (u < 10 && l != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(u + '0');
+			}
+			else if (u >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((u / 10) + '0');
+				_putchar((u % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
