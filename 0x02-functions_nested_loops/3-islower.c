176199be0780050d4330158e432b5fd7aabb1d7c
Oni Kehinde <kehindeoni89@gmail.com>
2023-02-22 02:57:57 +0100

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,10 +7,8 @@
  * Return: 1 if the character is lowercase, otherwise 0
  */
 int _islower(int c)
-
 {
-		
-	if (c >= 'a' && c <='z')
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
 		return (0);
