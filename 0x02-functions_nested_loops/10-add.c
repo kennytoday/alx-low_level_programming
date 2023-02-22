61b21623881229295d6995232d0b96765065d908
Oni Kehinde <kehindeoni89@gmail.com>
2023-02-22 12:17:20 +0100

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - Entry point
+ * @int: program to add two integers
+ * Return: Always 0
+ */
+int add(int, int)
+{
+	int a, b, c;
+
+	c = (a + b);
+	_putchar(c);
+	return (c);
+}
+
