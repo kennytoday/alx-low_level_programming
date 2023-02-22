a84634790ca1f0740d19e7ad027932a48400176d
Oni Kehinde <kehindeoni89@gmail.com>
2023-02-22 01:42:44 +0100

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		_putchar(alpha);
+	_putchar("\n);
+	return (0);
+}
