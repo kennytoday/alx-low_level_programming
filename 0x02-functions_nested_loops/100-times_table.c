60e82e40bc3511ceca98c69e8117fb1b22f21b62
Oni Kehinde <kehindeoni89@gmail.com>
2023-02-23 00:38:06 +0100

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_times_table - Entry point
+ *
+ * @n: C program
+ *
+ */
+void print_times_table(int n)
+{
+	int i, j, k;
+
+	if (n < 0 && n > 15)
+	{
+		for (i = 0; i < 0; i++)
+		{
+			_putchar('0');
+
