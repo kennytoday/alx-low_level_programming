35914c21e4e1560f465db50299864f85553ca3f2
Oni Kehinde <kehindeoni89@gmail.com>
2023-02-22 02:50:31 +0100

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description - program to print lower case characters
+ *
+ * Return: 1 if the character is lowercase, otherwise 0
+ */
+int _islower(int c)
+
+{
+	char c,
+	
+	if (c >= 'a'; && c <='z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
