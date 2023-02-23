0122b51a0bfae9ac75471b16f3d6a5c0feadb532
Oni Kehinde <kehindeoni89@gmail.com>
2023-02-24 00:10:46 +0100

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,14 +1,12 @@
 #include "main.h"
 /**
  * more_numbers - Entry point
- * @void: program that print 10 times
+ * @void: c program
  */
-
 void more_numbers(void)
 {
-	int a;
-	int b;
-	
+	int a, b;
+
 	for (a = 0; a < 10; a++)
 	{
 		for (b = 0; b <= 14; b++)
