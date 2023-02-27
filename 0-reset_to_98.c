4dcdc3f21c839555e44ef4e9b79a08aa8533cb5b
Oni Kehinde <kehindeoni89@gmail.com>
2023-02-27 20:20:26 +0100

    0-reset_to_98.c

---
+++ 0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Main Entry
+ * @*n: c program that points to integer
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+	int a;
+	int *n;
+
+	*n = 98;
+	n = &a;
+	_putchar("value of a is : %p\n", *n);
+
+	return (0);
+}
