b2fc8f77800c4da8f1b1f2e422ddd45ae5fd3665
Oni Kehinde <kehindeoni89@gmail.com>
2023-02-27 22:06:32 +0100

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - main entry
+ * @s: program to print reverse string
+ * Return: 0
+ *
+ */
+void print_rev(char *s)
+{
+	int measure = 0;
+	int a;
+
+	while (*s != '\0')
+	{
+		measure++;
+		s++;
+	}
+	s--;
+	for (a = measure; a > 0, a--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
