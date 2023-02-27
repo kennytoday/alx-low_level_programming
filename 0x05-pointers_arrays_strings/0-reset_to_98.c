1cf0f57e39930c1bc777444d2a4cd614e9e8c974
Oni Kehinde <kehindeoni89@gmail.com>
2023-02-27 20:41:12 +0100

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,15 +3,9 @@
 /**
  * reset_to_98 - Main Entry
  * @n: c program that points to integer
- * Return: 0
+ * Return: n
  */
 void reset_to_98(int *n)
 {
-	int a;
-	int *n;
 	*n = 98;
-	n = &a;
-	_putchar("value of a is : %p\n", *n);
-
-	return (0);
 }
