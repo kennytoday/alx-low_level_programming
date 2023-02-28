b8c96ce71ccea4bdaabf5097c13aaaf9be60b0f6
Oni Kehinde <kehindeoni89@gmail.com>
2023-02-28 11:49:45 +0100

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,4 +6,18 @@
  */
 void puts_half(char *str)
 {
+	char *str, n;
 
+	char *str = "length_of_the_screen";
+	int length = strlen(str);
+
+	n = ('length_of_the_string' - 1) / 2;
+	printf("Enter a string: %d\n");
+	scanf("%s", str);
+	for (n = 0; str[n] != '\0'; n++)
+	{
+		print("length_of_the_string: %s\n");
+		}
+
+	return (0);
+}
